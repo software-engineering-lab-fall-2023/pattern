d14f5e3c11c4f9a9fa61aa4115b654cd08db4631
Pouya <poesmaili@gmail.com>
2023-11-22 23:56:27 +0330

    implement logic for handling input and output

--- src/main/java/edu/sharif/sel/Package.java
+++ src/main/java/edu/sharif/sel/Package.java
@@ -4,20 +4,29 @@ import edu.sharif.sel.state.State;
 import edu.sharif.sel.strategy.Strategy;
 
 public class Package {
-    private State state;
     private Strategy strategy;
+    private State state;
     private double weight;
 
-    public Package(State state, Strategy strategy) {
-        this.state = state;
-        this.strategy = strategy;
+    public Package(double weight) {
+        this.weight = weight;
     }
 
     public void setState(State state) {
         this.state = state;
+        System.out.println(state.getMessage());
     }
 
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
+    }
+
+    public double getPrice() {
+        return strategy.calculatePrice(weight);
+    }
+
+    public boolean nextState() {
+        state.next();
+        return state.end();
     }
 }
--- src/test/java/StateTest.java
+++ src/test/java/StateTest.java
@@ -8,13 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StateTest {
     @Test
     public void testInTransitState() {
-        State state = new InTransitState();
+        State state = new InTransitState(null);
         assertEquals("In Transit", state.getMessage());
     }
 
     @Test
     public void testDeliveredState() {
-        State state = new DeliveredState();
+        State state = new DeliveredState(null);
         assertEquals("Delivered", state.getMessage());
     }
 }
