2000bbbe755a57de70bcd55a3816534a8bb3f2c4
Pouya <poesmaili@gmail.com>
2023-11-22 23:31:20 +0330

    Implement State tests

---
+++ src/main/java/edu/sharif/sel/state/State.java
@@ -0,0 +1,5 @@
+package edu.sharif.sel.state;
+
+abstract public class State {
+    abstract public String getMessage();
+}
---
+++ src/test/java/StateTest.java
@@ -0,0 +1,20 @@
+import edu.sharif.sel.state.DeliveredState;
+import edu.sharif.sel.state.InTransitState;
+import edu.sharif.sel.state.State;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StateTest {
+    @Test
+    public void testInTransitState() {
+        State state = new InTransitState();
+        assertEquals("In Transit", state.getMessage());
+    }
+
+    @Test
+    public void testDeliveredState() {
+        State state = new DeliveredState();
+        assertEquals("Delivered", state.getMessage());
+    }
+}
